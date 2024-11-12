fb7c31f5397c7e49bc6a5adbf525d37b1b34527f
ayushsingh953 <ayushsingh35323@gmail.com>
2024-11-12 14:47:42 +0530

    connencted lpu to database

---
+++ lpu/index.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
+import express from "express"; 
+import {connectDB} from "@macbease-multiverse/native";
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+
+
+const port = process.env.PORT || 5050;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    console.log("connected to database!")
+    app.listen(port, () => {
+      console.log(`Server is listening to port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
